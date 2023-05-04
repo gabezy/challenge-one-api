87dd10b7743ef093d552c22da21669f4cbc5e0a0
gabezy <gabrielmoreira2705@gmail.com>
2023-05-04 00:39:54 -0300

    initial commit

---
+++ src/main/java/challenge/one/api/controller/Hello.java
@@ -0,0 +1,14 @@
+package challenge.one.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+    @GetMapping
+    public String helloWorld () {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/challenge/one/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.one.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
